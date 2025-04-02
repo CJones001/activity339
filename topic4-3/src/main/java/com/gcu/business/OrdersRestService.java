798625f16044436faedb2b74bcd9ba81d12e141a
Chance Jones <chancejones01@outlook.com>
2025-04-02 14:20:46 -0700

    Update OrdersRestService.java

--- topic4-3/src/main/java/com/gcu/business/OrdersRestService.java
+++ topic4-3/src/main/java/com/gcu/business/OrdersRestService.java
@@ -7,7 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.gcu.model.OrderModel;
 import com.gcu.model.OrderList;
 
