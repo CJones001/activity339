af30116ee9efce766deb1306feb4960c1c0cf9f1
Chance Jones <chancejones01@outlook.com>
2025-04-03 13:43:10 -0700

    Update OrderRowMapper.java

--- topic4-3/src/main/java/com/gcu/data/mapper/OrderRowMapper.java
+++ topic4-3/src/main/java/com/gcu/data/mapper/OrderRowMapper.java
@@ -2,16 +2,22 @@ package com.gcu.data.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.gcu.data.entity.OrderEntity;
 
 public class OrderRowMapper implements RowMapper<OrderEntity>
 { 
+	private static final Logger logger = LoggerFactory.getLogger(OrderRowMapper.class);
+
 	@Override
 	public OrderEntity mapRow(ResultSet rs, int rowNumber) throws SQLException 
 	{
+		logger.info("Entering OrderRowMapper.mapRow()");
+	        logger.info("In the OrderRowMapper.mapRow()");
+	        logger.info("Exiting OrderRowMapper.mapRow()");
 		return new OrderEntity(rs.getLong("ID"), rs.getString("ORDER_NO"), rs.getString("PRODUCT_NAME"), rs.getFloat("PRICE"), rs.getInt("QUANTITY"));    
 	}
  }
