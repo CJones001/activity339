efe39d0193b745f758b6ac4585783e964350c33b
Chance Jones <chancejones01@outlook.com>
2025-04-03 09:15:11 -0700

    Update SecurityBusinessService.java

--- topic4-3/src/main/java/com/gcu/business/SecurityBusinessService.java
+++ topic4-3/src/main/java/com/gcu/business/SecurityBusinessService.java
@@ -1,13 +1,18 @@
 package com.gcu.business;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityBusinessService
 {
+	private static final Logger logger = LoggerFactory.getLogger(OrdersBusinessService.class);
+
 	public boolean authenticate(String username, String password)
 	{
+		logger.info("Entering SecurityBusinessService.authenticate()");
 		System.out.println(String.format("Hello from the SecurityBusinessService with a username of %s and a password of %s", username, password));
+		logger.info("Exiting SecurityBusinessService.authenticate()");
 		return true;
 	}
 }
