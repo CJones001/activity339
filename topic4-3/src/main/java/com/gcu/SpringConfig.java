865558909d1d4ec71153d8eaea4cb73566ba711c
Chance Jones <chancejones01@outlook.com>
2025-04-03 13:32:53 -0700

    Update SpringConfig.java

--- topic4-3/src/main/java/com/gcu/SpringConfig.java
+++ topic4-3/src/main/java/com/gcu/SpringConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.context.annotation.ScopedProxyMode;
 //import org.springframework.web.context.annotation.RequestScope;
 //import org.springframework.web.context.annotation.SessionScope;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.gcu.business.OrdersBusinessService;
 import com.gcu.business.OrdersBusinessServiceInterface;
 
@@ -14,12 +15,17 @@ import com.gcu.business.OrdersBusinessServiceInterface;
 @Configuration
 public class SpringConfig
 {
+	private static final Logger logger = LoggerFactory.getLogger(SpringConfig.class);
+	
 	@Bean(name="ordersBusinessService", initMethod="init", destroyMethod="destroy")
 //	@Scope(value="prototype", proxyMode=ScopedProxyMode.TARGET_CLASS)
 //	@RequestScope
 //	@SessionScope
 	public OrdersBusinessServiceInterface getOrdersBusiness()
 	{
+		logger.info("Entering SpringConfig.getOrdersBusiness()");
+        	logger.info("In the SpringConfig.getOrdersBusiness()");
+        	logger.info("Exiting SpringConfig.getOrdersBusiness()");
 		return new OrdersBusinessService();
 	}
 }
