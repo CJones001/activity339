7ed3fa9a2ce905f8a1ce036d8ec73c4a835d1e82
Chance Jones <chancejones01@outlook.com>
2025-04-03 09:23:05 -0700

    Update LoginController.java

--- topic4-3/src/main/java/com/gcu/controller/LoginController.java
+++ topic4-3/src/main/java/com/gcu/controller/LoginController.java
@@ -11,7 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.gcu.business.OrdersBusinessServiceInterface;
 import com.gcu.business.SecurityBusinessService;
 import com.gcu.model.LoginModel;
@@ -21,6 +22,8 @@ import com.gcu.model.OrderModel;
 @RequestMapping("/login")
 public class LoginController
 {
+	private static final Logger logger = LoggerFactory.getLogger(OrdersBusinessService.class);
+	
 	@Autowired
 	private OrdersBusinessServiceInterface service;
 
@@ -30,34 +33,47 @@ public class LoginController
 	@GetMapping("/")
 	public String display(Model model)
 	{
-		// Display Login Form View
-		model.addAttribute("title", "Login Form");
-		model.addAttribute("loginModel", new LoginModel());
+		logger.info("Entering LoginController.display()");
+
+		try{
+			// Display Login Form View
+			model.addAttribute("title", "Login Form");
+			model.addAttribute("loginModel", new LoginModel());
+		}catch(Exception e){
+			logger.error("Error in LoginController.display()");
+		}
+
+		logger.info("Exiting LoginController.display()"):
 		return "login";
 	}
 
 	@PostMapping("/doLogin")
 	public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model)
 	{
-        // Check for validation errors
-        if (bindingResult.hasErrors()) 
-        {
-        	model.addAttribute("title", "Login Form");
-            return "login";
-        }
-        
-        // Make a call to the Security Business Service
-        security.authenticate(loginModel.getUsername(), loginModel.getPassword());
-        
-        // Make a call to the Orders Business Service
-        service.test();
-        
+		logger.info("Entering LoginController.doLogin()");
+		logger.info("Hello from LoginController.doLogin()");
+	        // Check for validation errors
+	        if (bindingResult.hasErrors()) 
+	        {
+	        	model.addAttribute("title", "Login Form");
+			logger.error("Binding Result has errors, Exiting LoginController.doLogin()");
+	            	return "login";
+	        }
+	        
+	        // Make a call to the Security Business Service
+	        security.authenticate(loginModel.getUsername(), loginModel.getPassword());
+	        
+	        // Make a call to the Orders Business Service
+	        service.test();
+	        
 		// Get some Orders
 		List<OrderModel> orders = service.getOrders();
 
 		// Display the Orders View
 		model.addAttribute("title", "My Orders");
 		model.addAttribute("orders", orders);
+
+		logger.info("Exiting LoginController.doLogin()");
 		return "orders";
 	}
 }
